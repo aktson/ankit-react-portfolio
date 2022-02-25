d4b313268231f287cde941758c7ed9f9c06b420e
Ankit Soni <akt_soni@yahoo.co.in>
2022-02-25 14:27:29 +0100

    form restyled and connected to server

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -36,10 +36,24 @@ function Contact() {
       <h2>Let's Work together</h2>
 
       <form ref={formRef} className="form" onSubmit={handleSubmit}>
-        <input type="text" name="user_name" className="input-form" placeholder="name" />
-        <input type="text" name="user_subject" className="input-form" placeholder="subject" />
-        <input type="text" name="user_email" className="input-form" placeholder="email" />
-        <textarea name="message" className="textarea h-24 input-form" placeholder="your message" />
+        <div className="form-control  w-full ">
+          <label htmlFor="name" className="mb-2">
+            <span className="label-text ">Name</span>
+          </label>
+          <input type="text" name="user_name" className="input-form" placeholder="name" />
+        </div>
+        <div className="form-control  w-full">
+          <label htmlFor="email" className="mb-2">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="text" name="user_email" className="input-form" placeholder="email" />{" "}
+        </div>
+        <div className="form-control h-full  w-full">
+          <label htmlFor="message" className="mb-2">
+            <span className="label-text">Message </span>
+          </label>
+          <textarea name="message" className="textarea h-24 input-form" placeholder="your message" />{" "}
+        </div>
         <button className="btn-primary btn-md btn w-full" type="submit">
           Send
         </button>
@@ -49,65 +63,3 @@ function Contact() {
 }
 
 export default Contact;
-
-{
-  /* <form
-ref={formRef}
-className="form-control p-8 flex gap-5 items-start shadow-2xl w-full md:w-6/12 xl:w-3/12  rounded-box bg-base-100"
-onSubmit={handleSubmit((data, e) => {
-  console.log(data);
-})}>
-<div className="form-control  w-full ">
-  <label htmlFor="name" className="mb-2">
-    <span className="label-text ">Name {displayError(errors.name?.message)}</span>
-  </label>
-  <input
-    {...register("name", {
-      required: " name is required",
-      minLength: {
-        value: 4,
-        message: "name must be atleast 4 characters",
-      },
-    })}
-    id="name"
-    className="input-form"
-    placeholder="name"
-  />
-</div>
-<div className="form-control  w-full">
-  <label htmlFor="email" className="mb-2">
-    <span className="label-text">Email {displayError(errors.email?.message)}</span>
-  </label>
-  <input
-    name="user_email"
-    id="email"
-    className="input-form"
-    placeholder="email"
-    {...register("email", {
-      required: "email is required",
-      pattern: {
-        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-        message: "please enter valid email ",
-      },
-    })}
-  />
-</div>
-<div className="form-control h-full  w-full">
-  <label htmlFor="message" className="mb-2">
-    <span className="label-text">Message {displayError(errors.message?.message)}</span>
-  </label>
-  <textarea
-    name="message"
-    className="textarea h-24 input-form"
-    placeholder="your message"
-    {...register("message", {
-      required: "message must be atleast 10 characters",
-      minLength: { value: 10, message: "message must be 10 characters" },
-    })}
-  />
-</div>
-<button className="btn-primary btn-md btn w-full" type="submit">
-  Send
-</button>
-</form> */
-}
