8f308e64337844fa8181769942ababc0ee8f8a3b
Ankit Soni <akt_soni@yahoo.co.in>
2022-03-09 22:26:43 +0100

    icons from indicators removed

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -1,37 +1,13 @@
 import { Link } from "react-scroll";
-import { FaHome, FaInfo, FaProjectDiagram, FaRegIdBadge, FaDotCircle } from "react-icons/fa";
+import { FaDotCircle } from "react-icons/fa";
 
 function ScrollIndicator() {
   return (
-    <div className="fixed right-2 sm:right-5 top-16 ">
-      {/* indicator with icons for small to larger screens */}
-      <ul class=" bg-base-100 shadow menu rounded-lg hidden sm:flex ">
+    <div className="fixed right-2 sm:right-6 bottom-40 " id="indicatior-container">
+      <ul class=" flex flex-col gap-6 ">
         <li>
           <Link activeClass="active-indicator" to="hero" spy={true} smooth={true} duration={500}>
-            <FaHome className="indicator-icons" />
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active-indicator" to="about" spy={true} smooth={true} duration={500}>
-            <FaInfo className="indicator-icons" />
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active-indicator" to="projects" spy={true} smooth={true} duration={500}>
-            <FaProjectDiagram className="indicator-icons" />
-          </Link>
-        </li>
-        <li>
-          <Link activeClass="active-indicator" to="contact" spy={true} smooth={true} duration={500}>
-            <FaRegIdBadge className="indicator-icons" />
-          </Link>
-        </li>
-      </ul>
-      {/* Small button indicators for mobile */}
-      <ul class=" flex flex-col gap-4 sm:hidden ">
-        <li>
-          <Link activeClass="active-indicator" to="hero" spy={true} smooth={true} duration={500}>
-            <FaDotCircle className="indicator-icons" />
+            <FaDotCircle className="indicator-icons " />
           </Link>
         </li>
         <li>
