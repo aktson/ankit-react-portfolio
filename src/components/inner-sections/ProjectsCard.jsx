5ba9190a59d25133652f1435d860b974d8bbcee1
Ankit Soni <akt_soni@yahoo.co.in>
2022-04-05 09:24:40 +0200

    project card updated

--- src/components/inner-sections/ProjectsCard.jsx
+++ src/components/inner-sections/ProjectsCard.jsx
@@ -14,38 +14,6 @@ function ProjectsCard({ id, title, img, stack, summary, url, github, stacks }) {
   });
 
   return (
-    // <div className="hero shadow-xl h-80 w-80 md:h-96 " style={{ backgroundImage: `url(${img})` }} id="project-tile">
-    //   <div className="hero-overlay bg-opacity-60"></div>
-    //   <div className="text-center text-neutral-content hero-content p-4 ">
-    //     <div className="max-w-lg ">
-    //       <h3 className=" mb-5  font-bold text-slate-200 ">{title}</h3>
-    //       <p className="flex w-full items-center justify-center my-5 h-20 border p-2">{stack}</p>
-    //       <button className="btn btn-accent btn-base " onClick={handleClick} key={id}>
-    //         View
-    //       </button>
-    //       {isOpen && (
-    //         <ModalProjects open={isOpen} title={title} summary={summary} img={img} url={url} stack={stack} onClose={() => setIsOpen(!isOpen)} />
-    //       )}
-    //     </div>
-    //   </div>
-
-    // <div class="card lg:w-96 w-80 bg-base-100 drop-shadow-2xl ">
-    //   <figure>
-    //     <img src={img} alt={title} />
-    //   </figure>
-    //   <div class="card-body">
-    //     <h3 class="card-title">{title}</h3>
-    //     <p className="h-10">{stack}</p>
-    //     <div class="card-actions justify-end">
-    //       <button class="btn btn-primary" onClick={handleClick} key={id}>
-    //         View
-    //       </button>
-    //       {isOpen && (
-    //         <ModalProjects open={isOpen} title={title} summary={summary} img={img} url={url} stack={stack} onClose={() => setIsOpen(!isOpen)} />
-    //       )}
-    //     </div>
-    //   </div>
-    // </div>
     <div className="hero shadow-xl max-w-lg sm:h-96 sm:w-96 h-96 w-80 items-end card " style={{ backgroundImage: `url(${img})` }}>
       <div className=" w-full bg-base-100 p-5 relative">
         <h3>{title}</h3>
