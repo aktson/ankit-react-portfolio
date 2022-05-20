1d47a162f62be4f45cea0937dcb4f199d09203f1
Ankit Soni <akt_soni@yahoo.co.in>
2022-05-20 11:02:44 +0200

    hero restyled

---
+++ src/components/inner-sections/Pattern.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import pattern from "../../assets/try.jpg";
+
+function Pattern() {
+	return (
+		<>
+			<img src={pattern} alt="" className="pattern" />
+		</>
+	);
+}
+
+export default Pattern;
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,42 +1,44 @@
 import { Link } from "react-scroll";
 import { motion, AnimatePresence } from "framer-motion";
 import Header from "../layout/Header";
+import Pattern from "../inner-sections/Pattern";
 
 function Hero() {
-  return (
-    <>
-      <section className=" flex flex-col justify-center h-screen " id="hero">
-        <Header />
-        <AnimatePresence>
-          <motion.div
-            className="container text-base-100  mx-auto px-1 "
-            id="hero-bg"
-            animate={{ opacity: 1, y: 0 }}
-            initial={{ opacity: 0, y: 50 }}
-            exit={{ opacity: 0, y: 0 }}
-            transition={{ duration: 1 }}>
-            <div className=" grid lg:grid-cols-2 text-primary justify-items-center ">
-              <div className="p-2">
-                <h1>
-                  Hei, <br /> Jeg er <span className="text-primary">Ankit Soni, </span> <br /> Front-end utvikler.
-                </h1>
-                <Link className="btn btn-primary btn-md sm:btn-wide " to="about" smooth={true} duration={500}>
-                  Se mitt arbeid
-                </Link>
-              </div>
-            </div>
-          </motion.div>
-        </AnimatePresence>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className=" flex flex-col justify-center h-screen " id="hero">
+				<Header />
+
+				<AnimatePresence>
+					<motion.div
+						className="container text-base-100  mx-auto px-1 relative "
+						id="hero-bg"
+						animate={{ opacity: 1, y: 0 }}
+						initial={{ opacity: 0, y: 50 }}
+						exit={{ opacity: 0, y: 0 }}
+						transition={{ duration: 1 }}>
+						<div className=" grid lg:grid-cols-1 text-primary lg:justify-items-center text-center ">
+							<div className="p-2">
+								<h1>
+									Hei, <br /> Jeg er <span className="text-primary">Ankit Soni, </span> <br /> Front-end utvikler.
+								</h1>
+								<Link className="btn btn-primary btn-md sm:btn-wide " to="about" smooth={true} duration={500}>
+									Se mitt arbeid
+								</Link>
+							</div>
+						</div>
+					</motion.div>
+				</AnimatePresence>
+			</section>
+		</>
+	);
 }
 
 export default Hero;
 
 // hero without image
 {
-  /* <div className="hero min-h-screen ">
+	/* <div className="hero min-h-screen ">
   <div className="text-center hero-content">
     <div className="max-w-md">
       <h1 className="text-5xl font-bold">
@@ -56,5 +58,5 @@ export default Hero;
 }
 // hero with background image
 {
-  /*  */
+	/*  */
 }
