1881e1d83d98a3208012152aa2504c3d6322e127
Ankit Soni <akt_soni@yahoo.co.in>
2022-06-07 11:50:47 +0200

    loader added conditionally

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -11,6 +11,27 @@ function Projects() {
 		fetchData();
 	}, []);
 
+	const mappedData = data.map((items) => {
+		const item = items.attributes;
+		const projectId = items.id;
+		const image = item.image.data.attributes.url;
+		const stackImages = item.stacks.data;
+
+		return (
+			<ProjectsCard
+				id={projectId}
+				title={item.title}
+				img={image}
+				stack={item.stack}
+				summary={item.summary}
+				url={item.url}
+				github={item.github}
+				stacks={stackImages}
+				key={projectId}
+			/>
+		);
+	});
+
 	async function fetchData() {
 		const url = baseUrl + "api/projects?populate=*";
 		const response = await fetch(url);
@@ -19,47 +40,25 @@ function Projects() {
 		setLoading(false);
 	}
 
-	if (loading) {
-		return <Loader />;
-	} else {
-		return (
-			<section className=" py-16 sm:py-32 px-1" id="projects">
-				<div className="container flex flex-col justify-center mx-auto items-center">
-					<h2 className="">PROSJEKTER</h2>
-					<p className="max-w-3xl text-center mb-5">
-						I løpet av studiene har jeg jobbet med mange skole- og praksis prosjekter som kan finnes på
-						<a href="https://github.com/aktson?tab=repositories" target="_blank" rel="noreferrer" className="text-primary ml-1">
-							github
-						</a>
-						, her er noen av dem utviklet med forskjellige verktøy og teknologi
-					</p>
-					<div className="grid  md:grid-cols-2 grid-cols-1 mx-auto  gap-8  max-w-screen-5xl  my-8 p-2 ">
-						{data.map((items) => {
-							const item = items.attributes;
-							const projectId = items.id;
-							const image = item.image.data.attributes.url;
-
-							const stackImages = item.stacks.data;
-
-							return (
-								<ProjectsCard
-									id={projectId}
-									title={item.title}
-									img={image}
-									stack={item.stack}
-									summary={item.summary}
-									url={item.url}
-									github={item.github}
-									stacks={stackImages}
-									key={projectId}
-								/>
-							);
-						})}
-					</div>
-				</div>
-			</section>
-		);
-	}
+	return (
+		<section className=" py-16 sm:py-32 px-1" id="projects">
+			<div className="container flex flex-col justify-center mx-auto items-center">
+				<h2 className="">PROSJEKTER</h2>
+				<p className="max-w-3xl text-center mb-5">
+					I løpet av studiene har jeg jobbet med mange skole- og praksis prosjekter som kan finnes på
+					<a href="https://github.com/aktson?tab=repositories" target="_blank" rel="noreferrer" className="text-primary ml-1">
+						github
+					</a>
+					, her er noen av dem utviklet med forskjellige verktøy og teknologi
+				</p>
+				{loading ? (
+					<Loader />
+				) : (
+					<div className="grid  md:grid-cols-2 grid-cols-1 mx-auto  gap-8  max-w-screen-5xl  my-8 p-2 ">{mappedData}</div>
+				)}
+			</div>
+		</section>
+	);
 }
 
 export default Projects;
