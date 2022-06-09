17f0963534430dbdaaae0750a5ca12e298aede43
Ankit Soni <akt_soni@yahoo.co.in>
2022-06-09 09:43:25 +0200

    scrollbar styled

--- src/components/inner-sections/ModalProjects.jsx
+++ src/components/inner-sections/ModalProjects.jsx
@@ -14,14 +14,14 @@ function Modal({ title, stack, summary, img, url, github, open, onClose }) {
 					animate={{ y: 0 }}
 					exit={{ y: 0 }}
 					transition={{ type: "spring", bounce: 0.4, duration: 0.6 }}>
-					<div className="max-w-2xl bg-base-100 rounded-lg grid  grid-rows-auto md:grid-rows-2 ">
+					<div className="max-w-2xl  bg-base-100 rounded-lg grid  grid-rows-auto md:grid-rows-2 " style={{ maxHeight: "850px" }}>
 						<div className=" h-80 md:min-h-full" style={{ background: `url(${img}) no-repeat center`, backgroundSize: "cover" }}></div>
 						<div className="card-body bg-base-100">
 							<h3 className="card-title flex flex-col text-neutral">
 								<span className="flex justify-start text-2xl mb-2">{title}</span>
 								<div className=" badge badge-primary badge-outline p-5 ">{stack}</div>
 							</h3>
-							<p className="text-neutral  text-left mb-5 line-break overflow-y-auto">{summary}</p>
+							<p className="text-neutral  mb-5 line-break overflow-y-auto scrollbar">{summary}</p>
 							<div className="flex justify-between items-center">
 								<div className="flex gap-2">
 									<a
