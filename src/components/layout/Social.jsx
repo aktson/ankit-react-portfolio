0327a6f033b1cac8cb4eaa268fae820b31e285b7
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-08 21:47:39 +0100

    Update Social.jsx

--- src/components/layout/Social.jsx
+++ src/components/layout/Social.jsx
@@ -20,7 +20,7 @@ function Social() {
 					<Email />
 				</li>
 			</ul>
-			<div className="h-16 w-1 bg-accent my-1 mx-auto"></div>
+			<div className="h-16 w-0.5 bg-accent my-1 mx-auto"></div>
 		</div>
 	);
 }
