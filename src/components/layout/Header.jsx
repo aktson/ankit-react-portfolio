c783e5519b792abe0e37ad88488d4aac9aff3fd5
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-08 21:27:34 +0100

    navbar show function added onscroll

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect, useCallback } from "react";
 import LangContext from "../../context/LangContext";
 import { Link, animateScroll as scroll } from "react-scroll";
 import { SideBar } from "./mobile-nav/SideBar";
@@ -9,8 +9,33 @@ import darkLogo from "../../assets/logo-dark.svg";
 
 function Header() {
 	const { isEng, isChecked } = useContext(LangContext);
+
+	const [y, setY] = useState(document.scrollingElement.scrollHeight);
+	const [showNavBar, setShowNavBar] = useState("");
+
+	const handleNavigation = useCallback(
+		(e) => {
+			if (y > window.scrollY) {
+				setShowNavBar("");
+			} else if (y < window.scrollY) {
+				setShowNavBar("-translate-y-32");
+			}
+			setY(window.scrollY);
+		},
+		[y]
+	);
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleNavigation);
+
+		return () => {
+			window.removeEventListener("scroll", handleNavigation);
+		};
+	}, [handleNavigation]);
+
 	return (
-		<header className=" absolute lg:fixed top-0 left-0 right-0 flex items-center justify-center z-40 sm:py-4  shadow-xl sm:shadow-none">
+		<header
+			className={` absolute lg:fixed top-0 left-0 right-0 flex items-center justify-center z-40 sm:py-4  shadow-xl sm:shadow-none ${showNavBar} transition-all duration-300 `}>
 			<div className="container flex justify-around md:justify-between gap-2 p-4 py-4 items-center flex-col sm:flex-row sm:bg-warning sm:shadow-xl rounded-full ">
 				{/* logo */}
 				<figure className=" xl:w-96 p-2 ml-3" onClick={() => scroll.scrollToTop()}>
