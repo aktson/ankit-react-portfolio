a35742dc80efdb55235d211e884612891d38c7f5
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-10 15:25:19 +0100

    vercel removed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Analytics } from '@vercel/analytics/react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -8,7 +7,6 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
   document.getElementById('root')
 );
